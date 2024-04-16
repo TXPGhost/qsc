ae6a503bfd8e5e608f50827c489e0add2abc416d
Alexander Robertson <xanrob@me.com>
2024-04-16 18:38:06 -0400

    change to c

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <tree_sitter/api.h>
+
+const TSLanguage* tree_sitter_qs(void);
+
+int main(int argc, char* argv[]) {
+    TSParser parser = ts_parser_new();
+
+    return 0;
+}
